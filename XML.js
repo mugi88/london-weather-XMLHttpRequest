00cdaa1536b6293a1bed2371f8fbf71b92c28a3c
mugi88 <m211097268+1@gmail.com>
2019-12-25 19:21:25 +0900

    Fix

---
+++ XML.js
@@ -0,0 +1,24 @@
+//XMLHttpRequestで書きます！
+//参考：https://www.sejuku.net/blog/30245
+//参考：CORSポリシー:https://yukimonkey.com/fix-error/cros1/
+
+//Pythonでローカルサーバー：https://qiita.com/okhrn/items/4d3c74563154f191ba16
+
+var xhr = new XMLHttpRequest();
+var url = "https://api.openweathermap.org/data/2.5/weather?APPID=4b5774e9f3d2a07b84f0f2f88e486224&q=London";
+const PORT =  "mugi-MacBook-Pro.local";
+
+xhr.responseType = 'json';
+xhr.open("GET",url);
+xhr.send();
+
+xhr.onreadystatechange = function()
+{
+    if( xhr.readyState == 4 && xhr.status == 200 )
+    {
+        if( xhr.response )
+        {
+            console.log(xhr.response["weather"][0]["description"]);
+        }
+    }
+};
